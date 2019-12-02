f01e2f009d9b1ef9aaf4f159165f74a5b6d815f3
lemon <lemonfanvv@gmail.com>
2019-12-02 14:57:35 +0800

    添加文章部分逻辑

--- LeanCloudDemo/LCDemo/AddArticleViewController.swift
+++ LeanCloudDemo/LCDemo/AddArticleViewController.swift
@@ -11,6 +11,14 @@ import Eureka
 import LeanCloud
 
 
+struct AtricleModel {
+    var title : String = ""
+    var content : String = ""
+    var images : [UIImage] = []
+    var date : Date = Date()
+}
+
+
 class AddArticleViewController: FormViewController {
 
     
@@ -18,21 +26,22 @@ class AddArticleViewController: FormViewController {
         super.viewDidLoad()
         
         form
-            +++ TextRow(){[unowned self] in
+            +++ TextRow(){
                 $0.title = "标题"
                 $0.placeholder = "请输入"
                 $0.tag = "InputTitleRow"
-                $0.onChange { (row) in
-                   
-                }
             }
             +++ Section("内容")
-            <<< TextAreaRow(){ [unowned self] in
+            <<< TextAreaRow(){
                 $0.placeholder = "请输入"
                 $0.tag = "InputContentRow"
-                $0.onChange { (row) in
-                    let sign = row.value ?? ""
-                }
+            }
+            +++ Section("")
+            <<< ImageRow() {
+                $0.title = "选择图片"
+//                row.value = UIImage(named: "avatar")
+                $0.sourceTypes = [.PhotoLibrary, .Camera]
+                $0.tag = "ImageRow"
             }
             +++ Section("")
             <<< ButtonRow(){
@@ -40,18 +49,57 @@ class AddArticleViewController: FormViewController {
                 $0.onCellSelection { [unowned self] (cell, row) in
                     let textRow : TextAreaRow? = self.form.rowBy(tag: "InputContentRow")
                     let titleTow : TextRow? = self.form.rowBy(tag: "InputTitleRow")
+                    let imageRow : ImageRow? = self.form.rowBy(tag: "ImageRow")
                     guard let content = textRow?.value , let title = titleTow?.value else {return}
-                    self.save(title ,content: content)
+                    self.save(title ,content: content, image: imageRow?.value)
                 }
             }
         
     }
     
-    func save(_ title : String , content : String){
+    func save(_ title : String , content : String , image : UIImage?){
+    
         
-        let article = LCObject(className: "Articles")
         
-//        let todo.set()
+        do {
+            let article = LCObject(className: "Articles")
+            try article.set("title", value: title)
+            try article.set("content", value: content)
+            try article.set("date",value: Date())
+            try article.set("author", value: User.current.info!)
+            
+            if let data = image?.jpegData(compressionQuality: 0.1) {
+                
+                let file = LCFile.init(payload: .data(data: data))
+                _ = file.save {[weak self] result in
+                    switch result {
+                    case .success:
+                        print("文件保存完成。objectId：" + (file.objectId?.value ?? ""))
+//                        self?.saveInfo(file)
+                    case .failure(error: let error):
+                        // 保存失败，可能是文件无法被读取，或者上传过程中出现问题
+                        print(error)
+                    }
+                }
+                
+            }else{
+                
+            }
+            
+            
+            article.save {[weak article] (result) in
+                switch result {
+                    
+                case .success:
+                    print(article?.objectId?.value ?? "没有id")
+                case .failure(let error):
+                    print(error)
+                    
+                }
+            }
+        }catch {
+            print(error)
+        }
         
     }
 }
